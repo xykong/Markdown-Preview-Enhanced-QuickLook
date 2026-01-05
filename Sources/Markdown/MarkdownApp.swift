e068e591bb8c0a6167d800f2cf937e6b6e8c4d60
xy.kong <xy.kong@gmail.com>
2026-01-05 20:28:16 +0800

    feat: adjust host app default window size to match quicklook dimensions (800x600 min)

--- Sources/Markdown/MarkdownApp.swift
+++ Sources/Markdown/MarkdownApp.swift
@@ -5,6 +5,8 @@ struct MarkdownApp: App {
     var body: some Scene {
         DocumentGroup(newDocument: MarkdownDocument()) { file in
             MarkdownWebView(content: file.document.text, fileURL: file.fileURL)
+                .frame(minWidth: 800, idealWidth: 1000, maxWidth: .infinity,
+                       minHeight: 600, idealHeight: 800, maxHeight: .infinity)
         }
     }
 }
