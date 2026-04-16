6f085ab0d9da4b2487657833c09b12146535f500
xy.kong <xy.kong@gmail.com>
2026-04-17 02:12:39 +0800

    feat(shared): add reloadFile notification name

--- Sources/Shared/NotificationNames.swift
+++ Sources/Shared/NotificationNames.swift
@@ -2,9 +2,10 @@ import Foundation
 
 extension Notification.Name {
     static let toggleSearch = Notification.Name("toggleSearch")
-    static let exportHTML = Notification.Name("exportHTML")
-    static let exportPDF = Notification.Name("exportPDF")
-    static let toggleHelp = Notification.Name("toggleHelp")
-    static let zoomIn = Notification.Name("zoomIn")
-    static let zoomOut = Notification.Name("zoomOut")
+    static let exportHTML   = Notification.Name("exportHTML")
+    static let exportPDF    = Notification.Name("exportPDF")
+    static let toggleHelp   = Notification.Name("toggleHelp")
+    static let zoomIn       = Notification.Name("zoomIn")
+    static let zoomOut      = Notification.Name("zoomOut")
+    static let reloadFile   = Notification.Name("reloadFile")
 }
